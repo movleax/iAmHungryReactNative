2342d081e5783047f216335fffba6d72f1fd8d7e
movleax <6619780+movleax@users.noreply.github.com>
2019-02-18 15:24:15 -0800

    add react-native-vector-icons package. Add react-navigation package

--- android/app/src/main/java/com/iamhungry/MainActivity.java
+++ android/app/src/main/java/com/iamhungry/MainActivity.java
@@ -2,6 +2,13 @@ package com.iamhungry;
 
 import com.facebook.react.ReactActivity;
 
+// for react-native-gesture-handler for android
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +18,16 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "IAmHungry";
+    }
+
+    // for react-native-gesture-handler for android
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
