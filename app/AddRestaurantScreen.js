06afc656921e40d2f0648671b0d4e395f3153af2
movleax <6619780+movleax@users.noreply.github.com>
2019-02-20 11:39:48 -0800

    Change to search for establishments . Implement redux action to add a restaurant to store.

--- app/AddRestaurantScreen.js
+++ app/AddRestaurantScreen.js
@@ -15,17 +15,28 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 class AddRestaurantScreen extends Component {
     render() {
       return (
-        <View >
+        //<View >
           <GooglePlacesAutocomplete
             placeholder='Search'
             minLength={2} // minimum length of text to search
-            autoFocus={false}
+            autoFocus={true}
             returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
             listViewDisplayed='auto'    // true/false/undefined
             fetchDetails={true}
             renderDescription={row => row.description} // custom description render
             onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-            console.log(data, details);
+              
+              var restaurantObj = {
+                name: details.name,
+                address: details.formatted_address,
+                location: details.geometry.location,
+                phone: details.formatted_phone_number,
+                price_level: details.price_level,
+                rating: details.rating,
+                hours_weekly: details.opening_hours.weekday_text,
+              }
+
+              this.props.addRestaurantEstablishment(restaurantObj);
             }}
 
             getDefaultValue={() => ''}
@@ -34,7 +45,7 @@ class AddRestaurantScreen extends Component {
             // available options: https://developers.google.com/places/web-service/autocomplete
             key: this.props.GOOGLE_MAPS_APIKEY,
             language: 'en', // language of the results
-            types: '(cities)' // default: 'geocode'
+            types: 'establishment' // default: 'geocode'
             }}
 
             styles={{
@@ -49,7 +60,7 @@ class AddRestaurantScreen extends Component {
             }
             }}
 
-            currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+            currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
             currentLocationLabel="Current location"
             nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
             GoogleReverseGeocodingQuery={{
@@ -62,13 +73,11 @@ class AddRestaurantScreen extends Component {
             }}
 
             filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            //predefinedPlaces={[homePlace, workPlace]}
+            predefinedPlaces={[]}
 
             debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-            renderLeftButton={()  => <Text> Left Button or Left Icon </Text>}
-            renderRightButton={() => <Text>Custom text after the input</Text>}
             />
-        </View>
+        //</View>
       );
     }
   }
@@ -82,8 +91,7 @@ function mapStateToProps(state){
 
 function mapDispatchToProps(dispatch){
     return {
-        testFunctionOne: () => dispatch({type:'TEST_ONE'}),
-        testFunctionTwo: () => dispatch({type:'TEST_TWO'}),
+        addRestaurantEstablishment: (restaurantObj) => dispatch({type:'ADD_RESTAURANT', payload: restaurantObj}),
     }
 }
 
