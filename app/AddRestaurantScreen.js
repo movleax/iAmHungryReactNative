43f9f5b02001117a6b65364e80919285e960a8ed
movleax <6619780+movleax@users.noreply.github.com>
2019-04-03 15:33:53 -0700

    Add functionality to AddRestaurant that will post a new restaurant to the server, then it will ask the server for the updated list. Need to navigate back to the previous navigation in our async function, otherwise the restaurant_list will update after the fact.

--- app/AddRestaurantScreen.js
+++ app/AddRestaurantScreen.js
@@ -11,103 +11,108 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {connect} from 'react-redux';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import ServerCommunication from './ServerCommunication';
+import Loading from './Loading';
+
 
 class AddRestaurantScreen extends Component {
   static navigationOptions = {
     title: 'Add Restaurant',
   };
 
-    _addRestaurant(restaurantDetails)
-    {
-      // check to see if the restaurant already exists in the store
-      if(this.props.restaurant_list.some(restaurant => restaurant.id === restaurantDetails.place_id))
-      {
-        return;
-      }
+  constructor(props)
+  {
+    super(props);
 
-      var restaurantObj = {
-        name: restaurantDetails.name,
-        address: restaurantDetails.formatted_address,
-        location: restaurantDetails.geometry.location,
-        phone: restaurantDetails.formatted_phone_number,
-        price_level: restaurantDetails.price_level,
-        rating: restaurantDetails.rating,
-        hours_weekly: restaurantDetails.opening_hours.weekday_text,
-        id: restaurantDetails.place_id,
-      }
-
-      // add this restaurant to the end of the restaurant list prop.
-      const newRestaurantList = this.props.restaurant_list.concat(restaurantObj);
-
-      console.log(newRestaurantList);
-
-      // call our action to update the redux store
-      this.props.addRestaurant(newRestaurantList);
-
-      
-    }
-
-    render() {
-      return (
-        //<View >
-          <GooglePlacesAutocomplete
-            placeholder='Search'
-            minLength={2} // minimum length of text to search
-            autoFocus={true}
-            returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-            listViewDisplayed='auto'    // true/false/undefined
-            fetchDetails={true}
-            renderDescription={row => row.description} // custom description render
-            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-              console.log(data);
-              console.log(details);
-              this._addRestaurant(details);
-              this.props.navigation.state.params.RefreshParentScreen();
-              this.props.navigation.goBack();
-            }}
-
-            getDefaultValue={() => ''}
-
-            query={{
-            // available options: https://developers.google.com/places/web-service/autocomplete
-            key: this.props.GOOGLE_MAPS_APIKEY,
-            language: 'en', // language of the results
-            types: 'establishment' // default: 'geocode'
-            }}
-
-            styles={{
-            textInputContainer: {
-                width: '100%'
-            },
-            description: {
-                fontWeight: 'bold'
-            },
-            predefinedPlacesDescription: {
-                color: '#1faadb'
-            }
-            }}
-
-            currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
-            currentLocationLabel="Current location"
-            nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-            GoogleReverseGeocodingQuery={{
-            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-            }}
-            GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: 'distance',
-            types: 'restaurant'
-            }}
-
-            filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            predefinedPlaces={[]}
-
-            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-            />
-        //</View>
-      );
+    this.state = {
+      isLoading: false,
+      showErrorMsg: false
     }
   }
+
+  async _addRestaurant(restaurantDetails)
+  {
+    // check to see if the restaurant already exists in the store; return if this is the case
+    if(this.props.restaurant_list.some(restaurant => restaurant.id === restaurantDetails.place_id))
+    {
+      return;
+    }
+
+    this.setState({isLoading: true, showErrorMsg: false});
+
+    if((await ServerCommunication.PostRestaurantToServer(restaurantDetails)).status != 200){
+      this.setState({isLoading: false, showErrorMsg: true});
+      return;
+    }
+
+    if((await ServerCommunication.RetrieveAndStoreRestaurantList()).status != 200){
+      this.setState({isLoading: false, showErrorMsg: true});
+      return;
+    }
+
+    this.setState({isLoading: false});
+    this.props.navigation.state.params.RefreshParentScreen();
+    this.props.navigation.goBack();
+  }
+
+  render() {
+
+    return (   
+      <GooglePlacesAutocomplete
+      placeholder='Search'
+      minLength={2} // minimum length of text to search
+      autoFocus={true}
+      returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+      listViewDisplayed='auto'    // true/false/undefined
+      fetchDetails={true}
+      renderDescription={row => row.description} // custom description render
+      onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+        // console.log(data);
+        // console.log(details);
+        this._addRestaurant(details);
+      }}
+
+      getDefaultValue={() => ''}
+
+      query={{
+      // available options: https://developers.google.com/places/web-service/autocomplete
+      key: this.props.GOOGLE_MAPS_APIKEY,
+      language: 'en', // language of the results
+      types: 'establishment' // default: 'geocode'
+      }}
+
+      styles={{
+      textInputContainer: {
+          width: '100%'
+      },
+      description: {
+          fontWeight: 'bold'
+      },
+      predefinedPlacesDescription: {
+          color: '#1faadb'
+      }
+      }}
+
+      currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
+      currentLocationLabel="Current location"
+      nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+      GoogleReverseGeocodingQuery={{
+      // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+      }}
+      GooglePlacesSearchQuery={{
+      // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+      rankby: 'distance',
+      types: 'restaurant'
+      }}
+
+      filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+      predefinedPlaces={[]}
+
+      debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+      />
+    );
+  }
+}
   
 
 function mapStateToProps(state){
@@ -119,7 +124,7 @@ function mapStateToProps(state){
 
 function mapDispatchToProps(dispatch){
     return {
-        addRestaurant: (restaurantArray) => dispatch({type:'ADD_RESTAURANT', payload: restaurantArray}),
+      setRestaurantsList: (restaurantsList) => dispatch({type:'SET_RESTAURANT_LIST', payload: restaurantsList}),
     }
 }
 
