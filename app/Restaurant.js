a318800498020e873376f3b3b2d63540e5e6838b
movleax <6619780+movleax@users.noreply.github.com>
2019-04-16 10:06:37 -0700

    the format of the open hours weekly starts on monday, so the retrieval of the day  from the array should be shifted using some basic math.

--- app/Restaurant.js
+++ app/Restaurant.js
@@ -14,7 +14,7 @@ class Restaurant extends React.PureComponent {
             <Text style={this.props.nameStyle}>{this.props.name}</Text>
             <Text style={this.props.addressStyle}>{this.props.address}</Text>
             <Text style={this.props.phoneStyle}>Phone#: {this.props.phone}</Text>
-            <Text style={this.props.hoursStyle}>Hours today: {this.props.hours_weekly[new Date().getDay()]}</Text>
+            <Text style={this.props.hoursStyle}>Hours today: {this.props.hours_weekly[(new Date().getDay()+6)%7]}</Text>
             <Text style={this.props.priceStyle}>Price: {this.props.price_level > 0 ? "$".repeat(this.props.price_level) : 'free'}</Text>
             <Text style={this.props.ratingStyle}>Rating: {this.props.rating} / 5</Text>
         </View>
