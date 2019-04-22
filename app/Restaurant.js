f4dfaec3a738aa1654883b723bad1b3b69a58f6c
movleax <6619780+movleax@users.noreply.github.com>
2019-04-22 13:24:53 -0700

    because some constraints are loosened, restaurant componenet will display "N/A" if a value is null from the database>

--- app/Restaurant.js
+++ app/Restaurant.js
@@ -13,10 +13,10 @@ class Restaurant extends React.PureComponent {
         <View style={this.props.containerStyle}>
             <Text style={this.props.nameStyle}>{this.props.name}</Text>
             <Text style={this.props.addressStyle}>{this.props.address}</Text>
-            <Text style={this.props.phoneStyle}>Phone#: {this.props.phone}</Text>
-            <Text style={this.props.hoursStyle}>Hours today: {this.props.hours_weekly[(new Date().getDay()+6)%7]}</Text>
-            <Text style={this.props.priceStyle}>Price: {this.props.price_level > 0 ? "$".repeat(this.props.price_level) : 'free'}</Text>
-            <Text style={this.props.ratingStyle}>Rating: {this.props.rating} / 5</Text>
+            <Text style={this.props.phoneStyle}>Phone#: {this.props.phone ? this.props.phone : "N/A"}</Text>
+            <Text style={this.props.hoursStyle}>Hours today: {this.props.hours_weekly ? this.props.hours_weekly[(new Date().getDay()+6)%7] : "N/A"}</Text>
+            <Text style={this.props.priceStyle}>Price: {this.props.price_level >= 0 ? (this.props.price_level > 0 ? "$".repeat(this.props.price_level) : 'free' ): "N/A"}</Text>
+            <Text style={this.props.ratingStyle}>Rating: {this.props.rating ? this.props.rating : "N/A"} / 5</Text>
         </View>
       );
     }
